fd65cf495b9c5cf362405484e1f7cc511d2eb7c9
Zhang Guo Bin <zgbzsu2008@163.com>
2019-04-29 12:06:56 +0800

    object_pool op_queue test

---
+++ object_pool.hpp
@@ -0,0 +1,112 @@
+#ifndef BOOST_ASIO_DETAIL_OBJECT_POOL_HPP
+#define BOOST_ASIO_DETAIL_OBJECT_POOL_HPP
+
+#include "noncopyable.hpp"
+
+namespace boost::asio::detail
+{
+template <typename Object> class object_pool;
+
+class object_pool_access
+{
+ public:
+  template <typename Object> static Object* create() { return new Object; }
+
+  template <typename Object, typename Arg> static Object* create(Arg arg)
+  {
+    return new Object(arg);
+  }
+
+  template <typename Object> static void destory(Object* o) { delete o; }
+
+  template <typename Object> static Object*& next(Object* o) { return o->next_; }
+
+  template <typename Object> static Object*& prev(Object* o) { return o->prev_; }
+};
+
+template <class Object> class object_pool : noncopyable
+{
+ public:
+  object_pool() : live_list_(0), free_list_(0) {}
+
+  ~object_pool()
+  {
+    destroy_list(live_list_);
+    destroy_list(free_list_);
+  }
+
+  Object* first() { return live_list_; }
+
+  Object* alloc()
+  {
+    Object* o = free_list_;
+    if (o) {
+      free_list_ = object_pool_access::next(free_list_);
+    } else {
+      o = object_pool_access::create<Object>();
+    }
+
+    object_pool_access::next(o) = live_list_;
+    object_pool_access::prev(o) = 0;
+    if (live_list_) {
+      object_pool_access::prev(live_list_) = o;
+    }
+    live_list_ = o;
+
+    return o;
+  }
+
+  template <typename Arg> Object* alloc(Arg arg)
+  {
+    Object* o = free_list_;
+    if (o) {
+      free_list_ = object_pool_access::next(free_list_);
+    } else {
+      o = object_pool_access::create<Object>(arg);
+    }
+
+    object_pool_access::next(o) = live_list_;
+    object_pool_access::prev(o) = 0;
+    if (live_list_) {
+      object_pool_access::prev(live_list_) = o;
+    }
+    live_list_ = o;
+
+    return o;
+  }
+
+  void free(Object* o)
+  {
+    if (live_list_ == o) {  // head
+      live_list_ = object_pool_access::next(o);
+    }
+
+    if (object_pool_access::prev(o)) {  // next
+      object_pool_access::next(object_pool_access::prev(o)) = object_pool_access::next(o);
+    }
+
+    if (object_pool_access::next(o)) {  // prev
+      object_pool_access::prev(object_pool_access::next(o)) = object_pool_access::prev(o);
+    }
+
+    object_pool_access::next(o) = free_list_;  // insert front
+    object_pool_access::prev(o) = o;
+    free_list_ = o;
+  }
+
+ private:
+  void destroy_list(Object* list)
+  {
+    while (list) {
+      Object* o = list;
+      list = object_pool_access::next(o);
+      object_pool_access::destory(o);
+    }
+  }
+
+  Object* live_list_;
+  Object* free_list_;
+};
+}  // namespace boost::asio::detail
+
+#endif  //! BOOST_ASIO_DETAIL_OBJECT_POOL_HPP
--- test_event.hpp
+++ test_event.hpp
@@ -4,9 +4,31 @@
 
 #include "conditionally_enabled_event.hpp"
 #include "conditionally_enabled_mutex.hpp"
+#include "object_pool.hpp"
+#include "op_queue.hpp"
 #include "thread_group.hpp"
 
+#include <sys/socket.h>
+#include <unistd.h>
+
 using namespace boost::asio;
+
+class object
+{
+ public:
+  object(int fd) : fd_(fd) {}
+  int fd() const { return fd_; }
+  void destroy() {}
+
+  object* next_;
+  object* prev_;
+
+ private:
+  friend class scheduler;
+  friend class op_queue_access;
+  friend class object_pool_access;
+  int fd_;
+};
 
 class scheduler
 {
@@ -30,29 +52,30 @@ class scheduler
   int do_run_one(mutex::scoped_lock& lock)
   {
     while (true) {
-      if (queue_.empty()) {
+      if (op_queue_.empty()) {
         std::cout << "tid=" << std::this_thread::get_id() << " wait....\n";
         event_.clear(lock);
         event_.wait(lock);
       } else {
-        int top = queue_.front();
-        queue_.pop();
-        if (!queue_.empty()) {
+        object* top = op_queue_.front();
+        op_queue_.pop();
+        if (!op_queue_.empty()) {
           event_.unlock_and_signal_one(lock);
         } else {
           lock.unlock();
         }
         std::this_thread::sleep_for(std::chrono::seconds(1));
-        std::cout << "tid= " << std::this_thread::get_id() << " \tpop " << top << '\n';
+        std::cout << "tid= " << std::this_thread::get_id() << " \tpop " << top->fd() << '\n';
+        pool_.free(top);
         return 1;
       }
     }
   }
 
-  void post(int value)
+  void post(int fd)
   {
     mutex::scoped_lock lock(mutex_);
-    queue_.push(value);
+    op_queue_.push(pool_.alloc(fd));
     if (!event_.maybe_unlock_and_signal_one(lock)) {
       lock.unlock();
     }
@@ -61,7 +84,8 @@ class scheduler
  private:
   event event_;
   mutex mutex_;
-  std::queue<int> queue_;
+  detail::object_pool<object> pool_;
+  detail::op_queue<object> op_queue_;
 };
 
 inline int boost_asio_event()
